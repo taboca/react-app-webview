4bac4ec1798bb73e8e417271c96ce156c2a62962
Marcio <taboca@gmail.com>
2017-02-23 07:16:59 -0300

    Simplified script withouth examples built in

--- README.md
+++ README.md
@@ -8,3 +8,15 @@ The source is here for a few reasons:
 * Help Google Engineering to take an alternative review process 
 * Help the React Community in case of any recomendation from Google Play Policy system. 
 
+## Architecture (1.0-3.0)
+
+* It's based in the basic React-native app sample plus I have pasted the sample of Web view [1](https://github.com/facebook/react-native/blob/master/Examples/UIExplorer/js/WebViewExample.js). 
+
+## Main changes
+
+* Even thought I believe that nothing malicious was in the sample of React-native Facebook code [1](https://github.com/facebook/react-native/blob/master/Examples/UIExplorer/js/WebViewExample.js), yet I have simplified the web view, just to avoid the names that were in the code, such as the keyword "inject". 
+
+## References 
+
+* [1](https://github.com/facebook/react-native/blob/master/Examples/UIExplorer/js/WebViewExample.js)
+* 
--- index.android.js
+++ index.android.js
@@ -24,29 +24,6 @@ var TEXT_INPUT_REF = 'urlInput';
 var WEBVIEW_REF = 'webview';
 var DEFAULT_URL = 'http://www.lpeu.com.br/feed';
 
-class Image2 extends Component {
-  render() {
-    let pic = {
-     uri: this.props.source
-   };
-    return (
-        <Image source={pic} style={{width: 193, height: 110}}/>
-    );
-  }
-}
-
-class WebWrapper extends Component {
-  render() {
-    let data = {
-     uri: this.props.source
-   };
-    return (
-        <WebView source={{data}} style={{marginTop: 20}}           javaScriptEnabled={true}
- />
-    );
-  }
-}
-
 export default class AwesomeProject extends Component {
   render() {
     return (
@@ -56,7 +33,6 @@ export default class AwesomeProject extends Component {
     );
   }
 }
-
 
 class WebViewExample extends React.Component {
   state = {
@@ -149,318 +125,23 @@ class WebViewExample extends React.Component {
   };
 }
 
-class Button extends React.Component {
-  _handlePress = () => {
-    if (this.props.enabled !== false && this.props.onPress) {
-      this.props.onPress();
-    }
-  };
-
-  render() {
-    return (
-      <TouchableWithoutFeedback onPress={this._handlePress}>
-        <View style={styles.button}>
-          <Text>{this.props.text}</Text>
-        </View>
-      </TouchableWithoutFeedback>
-    );
-  }
-}
-
-class ScaledWebView extends React.Component {
-  state = {
-    scalingEnabled: true,
-  };
-
-  render() {
-    return (
-      <View>
-        <WebView
-          style={{
-            backgroundColor: BGWASH,
-            height: 200,
-          }}
-          source={{uri: 'https://facebook.github.io/react/'}}
-          scalesPageToFit={this.state.scalingEnabled}
-        />
-        <View style={styles.buttons}>
-        { this.state.scalingEnabled ?
-          <Button
-            text="Scaling:ON"
-            enabled={true}
-            onPress={() => this.setState({scalingEnabled: false})}
-          /> :
-          <Button
-            text="Scaling:OFF"
-            enabled={true}
-            onPress={() => this.setState({scalingEnabled: true})}
-          /> }
-        </View>
-      </View>
-    );
-  }
-}
-
-class MessagingTest extends React.Component {
-  webview = null
-
-  state = {
-    messagesReceivedFromWebView: 0,
-    message: '',
-  }
-
-  onMessage = e => this.setState({
-    messagesReceivedFromWebView: this.state.messagesReceivedFromWebView + 1,
-    message: e.nativeEvent.data,
-  })
-
-  postMessage = () => {
-    if (this.webview) {
-      this.webview.postMessage('"Hello" from React Native!');
-    }
-  }
-
-  render(): ReactElement<any> {
-    const {messagesReceivedFromWebView, message} = this.state;
-
-    return (
-      <View style={[styles.container, { height: 200 }]}>
-        <View style={styles.container}>
-          <Text>Messages received from web view: {messagesReceivedFromWebView}</Text>
-          <Text>{message || '(No message)'}</Text>
-          <View style={styles.buttons}>
-            <Button text="Send Message to Web View" enabled onPress={this.postMessage} />
-          </View>
-        </View>
-        <View style={styles.container}>
-          <WebView
-            ref={webview => { this.webview = webview; }}
-            style={{
-              backgroundColor: BGWASH,
-              height: 100,
-            }}
-            source={require('./messagingtest.html')}
-            onMessage={this.onMessage}
-          />
-        </View>
-      </View>
-    );
-  }
-}
-
-class InjectJS extends React.Component {
-  webview = null;
-  injectJS = () => {
-    const script = 'document.write("Injected JS ")';  // eslint-disable-line quotes
-    if (this.webview) {
-      this.webview.injectJavaScript(script);
-    }
-  }
-  render() {
-    return (
-      <View>
-        <WebView
-          ref={webview => { this.webview = webview; }}
-          style={{
-            backgroundColor: BGWASH,
-            height: 300,
-          }}
-          source={{uri: 'https://www.facebook.com'}}
-          scalesPageToFit={true}
-        />
-        <View style={styles.buttons}>
-          <Button text="Inject JS" enabled onPress={this.injectJS} />
-        </View>
-    </View>
-    );
-  }
-}
-
-
 var styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'white',
   },
-  addressBarRow: {
-    flexDirection: 'row',
-    padding: 8,
-  },
   webView: {
     backgroundColor: BGWASH,
     height: 350,
   },
-  addressBarTextInput: {
-    backgroundColor: BGWASH,
-    borderColor: 'transparent',
-    borderRadius: 3,
-    borderWidth: 1,
-    height: 24,
-    paddingLeft: 10,
-    paddingTop: 3,
-    paddingBottom: 3,
-    flex: 1,
-    fontSize: 14,
-  },
-  navButton: {
-    width: 20,
-    padding: 3,
-    marginRight: 3,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: BGWASH,
-    borderColor: 'transparent',
-    borderRadius: 3,
-  },
-  disabledButton: {
-    width: 20,
-    padding: 3,
-    marginRight: 3,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: DISABLED_WASH,
-    borderColor: 'transparent',
-    borderRadius: 3,
-  },
-  goButton: {
-    height: 24,
-    padding: 3,
-    marginLeft: 8,
-    alignItems: 'center',
-    backgroundColor: BGWASH,
-    borderColor: 'transparent',
-    borderRadius: 3,
-    alignSelf: 'stretch',
-  },
-  statusBar: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    paddingLeft: 5,
-    height: 22,
-  },
-  statusBarText: {
-    color: 'white',
-    fontSize: 13,
-  },
   spinner: {
     width: 20,
     marginRight: 6,
-  },
-  buttons: {
-    flexDirection: 'row',
-    height: 30,
-    backgroundColor: 'black',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-  },
-  button: {
-    flex: 0.5,
-    width: 0,
-    margin: 5,
-    borderColor: 'gray',
-    borderWidth: 1,
-    backgroundColor: 'gray',
-  },
+  }
 });
-
-const HTML = `
-<!DOCTYPE html>\n
-<html>
-  <head>
-    <title>Hello Static World</title>
-    <meta http-equiv="content-type" content="text/html; charset=utf-8">
-    <meta name="viewport" content="width=320, user-scalable=no">
-    <style type="text/css">
-      body {
-        margin: 0;
-        padding: 0;
-        font: 62.5% arial, sans-serif;
-        background: #ccc;
-      }
-      h1 {
-        padding: 45px;
-        margin: 0;
-        text-align: center;
-        color: #33f;
-      }
-    </style>
-  </head>
-  <body>
-    <h1>Hello Static World</h1>
-  </body>
-</html>
-`;
 
 exports.displayName = (undefined: ?string);
 exports.title = '<WebView>';
-exports.description = 'Base component to display web content';
-exports.examples = [
-  {
-    title: 'Simple Browser',
-    render(): React.Element<any> { return <WebViewExample />; }
-  },
-  {
-    title: 'Scale Page to Fit',
-    render(): React.Element<any> { return <ScaledWebView/>; }
-  },
-  {
-    title: 'Bundled HTML',
-    render(): React.Element<any> {
-      return (
-        <WebView
-          style={{
-            backgroundColor: BGWASH,
-            height: 100,
-          }}
-          source={require('./helloworld.html')}
-          scalesPageToFit={true}
-        />
-      );
-    }
-  },
-  {
-    title: 'Static HTML',
-    render(): React.Element<any> {
-      return (
-        <WebView
-          style={{
-            backgroundColor: BGWASH,
-            height: 100,
-          }}
-          source={{html: HTML}}
-          scalesPageToFit={true}
-        />
-      );
-    }
-  },
-  {
-    title: 'POST Test',
-    render(): React.Element<any> {
-      return (
-        <WebView
-          style={{
-            backgroundColor: BGWASH,
-            height: 100,
-          }}
-          source={{
-            uri: 'http://www.posttestserver.com/post.php',
-            method: 'POST',
-            body: 'foo=bar&bar=foo'
-          }}
-          scalesPageToFit={false}
-        />
-      );
-    }
-  },
-  {
-    title: 'Messaging Test',
-    render(): ReactElement<any> { return <MessagingTest />; }
-  },
-  {
-    title: 'Inject JavaScript',
-    render(): React.Element<any> { return <InjectJS />; }
-  },
-];
-
+exports.description = 'Displaying my own web content'
 
 AppRegistry.registerComponent('LpeuLive', () => AwesomeProject);
